a399e7e591a7e4e9be541298042866de8ae3abb6
mdnahidhasan <mdnahidhasanmunna999@gmail.com>
2025-08-29 21:11:09 +0600

    expanded

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:macapps/Screen/ProductGridViewScreen.dart';
-
-import 'Screen/ProductCreateScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +9,35 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: ProductGridViewscreen(),
-      title: 'Flutter CRUD APP',
+    return MaterialApp(home: MyHomePage(), title: 'Flutter CRUD APP');
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Counter App')),
+
+      body: Center(
+        child: Text(_counter.toString(), style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold)),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _counter = _counter + 1;
+          setState(() {});
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
