5883fa667a33daece237025de8b9cb85444d76ce
mdnahidhasan <mdnahidhasanmunna999@gmail.com>
2025-09-10 00:09:31 +0600

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:macapps/style.dart';
+import 'package:macapps/toDoPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,95 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MyHomePage(), title: 'Sum Calculator');
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  Map<String, String> FormData = {
-    'firstNumber': '',
-    'secondNumber': '',
-    'thirdNumber': '',
-  };
-
-  int sum = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    InputOnChange(field, inputValue) {
-      setState(() {
-        FormData.update(field, (value) => inputValue);
-      });
-    }
-
-    addAllNumbers() {
-      setState(() {
-        FormData.forEach((key, value) {
-          if (value.isNotEmpty) {
-            sum += int.parse(value);
-          }
-        });
-      });
-
-      return sum;
-    }
-
-    return Scaffold(
-      appBar: AppBar(title: const Text('Sum Calculator')),
-      body: Padding(
-        padding: EdgeInsets.all(40),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text(sum.toString(), style: appTextStyle()),
-            SizedBox(height: 20),
-            TextFormField(
-              onChanged: (value) {
-                InputOnChange('firstNumber', value);
-              },
-
-              decoration: AppInputDecoration(Text('Enter first number')),
-              keyboardType: TextInputType.number,
-            ),
-            SizedBox(height: 20),
-            TextFormField(
-              onChanged: (value) {
-                InputOnChange('secondNumber', value);
-              },
-              decoration: AppInputDecoration(Text('Enter second number')),
-              keyboardType: TextInputType.number,
-            ),
-
-            SizedBox(height: 20),
-            TextFormField(
-              onChanged: (value) {
-                InputOnChange('thirdNumber', value);
-              },
-              decoration: AppInputDecoration(Text('Enter third number')),
-              keyboardType: TextInputType.number,
-            ),
-
-            SizedBox(height: 20),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                style: appButtonStyle(),
-                onPressed: () {
-                  addAllNumbers();
-                },
-                child: Text('Calculate Sum'),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+    return MaterialApp(title: 'Todo App', home: ToDoPage());
   }
 }
--- lib/style.dart
+++ lib/style.dart
@@ -3,19 +3,15 @@ import 'package:flutter/material.dart';
 InputDecoration AppInputDecoration(label) {
   return InputDecoration(
     contentPadding: EdgeInsets.fromLTRB(20, 10, 10, 20),
-    fillColor: Colors.lime,
+    fillColor: Colors.white,
     filled: true,
 
     border: OutlineInputBorder(),
-    label: label,
-  );
-}
+    focusedBorder: OutlineInputBorder(
+      borderSide: BorderSide(color: Colors.greenAccent, width: 2),
+    ),
 
-TextStyle appTextStyle() {
-  return TextStyle(
-    fontSize: 25,
-    color: Colors.blue,
-    fontWeight: FontWeight.bold,
+    label: label,
   );
 }
 
@@ -23,11 +19,21 @@ ButtonStyle appButtonStyle() {
   return ElevatedButton.styleFrom(
     backgroundColor: Colors.greenAccent,
     foregroundColor: Colors.black,
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(30),
-    ),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
 
     padding: EdgeInsets.all(20),
-    textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    textStyle: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+  );
+}
+
+SizedBox SizeBox50(child) {
+  return SizedBox(
+    height: 50,
+    width: double.infinity,
+    child: Container(
+      alignment: Alignment.center,
+      padding: EdgeInsets.all(8),
+      child: child,
+    ),
   );
 }
